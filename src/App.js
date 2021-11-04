3582458c4ca64d35c86b404c64fe040219810e9e
nascott <nascott94@outlook.com>
2021-11-04 12:58:49 -0600

    reinstalling dependancies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import { HomePage } from "./pages/HomePage";
+
 function App() {
-  return <div className="App">React Shopify Store</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
