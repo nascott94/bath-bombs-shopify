952a32f2a88d4f8f32098369568917332c32fcf5
nascott <nascott94@outlook.com>
2021-11-04 11:46:19 -0600

    sstarting to set up shopContext

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,10 +1,68 @@
 import React, { Component } from "react";
 import Client from "shopify-buy";
 
-class shopContext extends Component {
+//creates a context object, used with provider
+const ShopContext = React.createContext();
+
+// Initializing a client to return content in the store's primary language
+const client = Client.buildClient({
+  domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
+});
+
+class ShopProvider extends Component {
+  //state starts with a default value when a Component mounts and then suffers from mutations in time
+  state = {
+    product: {},
+    products: [],
+    checkout: {},
+    isCartOpen: false,
+    isMenuOpen: false,
+  };
+
+  //keeps products in user cart checkout, save checkout id in local storage
+  createCheckout = async () => {
+    // Create an empty checkout
+    const checkout = await client.checkout.create();
+    localStorage.setItem("checkout-id", checkout.id);
+    this.setState({ checkout: checkout });
+  };
+
+  fetchCheckout = async () => {};
+
+  addItemtoCheckout = async () => {};
+
+  removeLineItem = async (lineItemIdsToRemove) => {};
+
+  fetchAllProducts = async () => {
+    // Fetch all products in your shop
+    const products = await client.product.fetchAll();
+    // Do something with the products
+    this.setState({ products: products });
+  };
+
+  //uses product url handle rather than id to fetch data
+  fetchProductWithHandle = async (handle) => {
+    // Fetch a single product by Handle
+    const product = await client.product.fetchByHandle(handle);
+    this.setState({ product: product });
+  };
+
+  closeCart = () => {};
+
+  openCart = () => {};
+
+  closeMenu = () => {};
+
+  openMenu = () => {};
+
   render() {
-    return <div></div>;
+    return <ShopContext.Provider>{this.props.client}</ShopContext.Provider>;
   }
 }
 
-export default shopContext;
+const ShopCosumer = ShopContext.Consumer;
+
+export { ShopCosumer, ShopContext };
+
+export default ShopProvider;
