5d6203944e738cd0f8971a69a07b0f9d7ce1b7b8
nascott <nascott94@outlook.com>
2021-11-04 10:21:17 -0600

    restarted, new inital push

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,82 +1,10 @@
 import React, { Component } from "react";
 import Client from "shopify-buy";
 
-const ShopContext = React.createContext();
-
-// Initializing a client to return content in the store's primary language
-//replace with your specific token and domain
-const client = Client.buildClient({
-  storefrontAccessToken: "dd4d4dc146542ba7763305d71d1b3d38",
-  domain: "graphql.myshopify.com",
-});
-
-class ShopProvider extends Component {
-  state = {
-    products: [],
-    product: {},
-    checkout: {},
-    isCartOpen: false,
-    test: "test",
-  };
-
-  componentDidMount() {
-    this.createCheckout();
-  }
-
-  createCheckout = async () => {
-    const checkout = await client.checkout.create();
-    this.setState({ checkout: checkout });
-  };
-
-  addItemToCheckout = async (variantId, quantity) => {
-    const lineItemsToAdd = [
-      {
-        variantId,
-        quantity: parseInt(quantity, 10),
-      },
-    ];
-    const checkout = await client.checkout.addLineItems(
-      this.state.checkout.id,
-      lineItemsToAdd
-    );
-    this.setState({ checkout: checkout });
-  };
-
-  fetchAllProducts = async () => {
-    const products = await client.product.fetchAll();
-    this.setState({ products: products });
-  };
-
-  fetchProductWithId = async (id) => {
-    const product = await client.product.fetch(id);
-    this.setState({ product: product });
-  };
-
-  closeCart = () => {
-    this.setState({ isCartOpen: false });
-  };
-  openCart = () => {
-    this.setState({ isCartOpen: true });
-  };
-
+class shopContext extends Component {
   render() {
-    return (
-      <ShopContext.Provider
-        value={{
-          ...this.state,
-          addItemToCheckout: this.addItemToCheckout,
-          fetchAllProduct: this.fetchAllProducts,
-          fetchProductWithId: this.fetchProductWithId,
-          closeCart: this.closeCart,
-          openCart: this.openCart,
-        }}
-      >
-        {this.props.children}
-      </ShopContext.Provider>
-    );
+    return <div></div>;
   }
 }
 
-const ShopConsumer = ShopContext.Consumer;
-export { ShopConsumer, ShopContext };
-export default ShopProvider;
+export default shopContext;
--- src/pages/HomePage.js
+++
@@ -1,7 +0,0 @@
-import React, { useContext, useEffect } from "react";
-import { ShopContext } from "../context/shopContext";
-
-export const HomePage = () => {
-  const { test } = useContext(ShopContext);
-  return <> Home {test}</>;
-};
