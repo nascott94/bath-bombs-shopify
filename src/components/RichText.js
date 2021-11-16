949c1ef4502c61ac2cb28d032be747bfb9e96171
nascott <nascott94@outlook.com>
2021-11-16 11:31:14 -0700

    finished styling

--- src/components/RichText.js
+++ src/components/RichText.js
@@ -3,10 +3,11 @@ import { Box, Heading, Text, Center } from "@chakra-ui/react";
 
 export const RichText = ({ heading, text }) => {
   return (
-    <Box p="1rem">
-      <Center display="flex" flexDir="column" textAlign="center"></Center>
-      <Heading py="2.5rem">{heading && heading}</Heading>
-      <Text pb="2rem">{text && text}</Text>
+    <Box p={4}>
+      <Center display="flex" flexDir="column" textAlign="center">
+        <Heading py={10}>{heading}</Heading>
+        {text ? <Text pb={10}>{text}</Text> : null}
+      </Center>
     </Box>
   );
 };
