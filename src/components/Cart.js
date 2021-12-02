d7b720c292a1f69d9f8c2c6f340645df1989678e
nascott <nascott94@outlook.com>
2021-12-02 10:10:20 -0700

    updated cart.js to work with netlify

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -11,80 +11,104 @@ import {
   DrawerCloseButton,
   Button,
   Grid,
-  Flex,
-  Image,
-  Text,
-  Link,
   Box,
+  Text,
+  Image,
+  SimpleGrid,
+  Link,
 } from "@chakra-ui/react";
-
 import { CloseIcon } from "@chakra-ui/icons";
 
 export const Cart = () => {
   const { isCartOpen, closeCart, checkout, removeLineItem } =
     useContext(ShopContext);
 
-  // console.log(checkout);
-
   return (
     <>
       <Drawer
         isOpen={isCartOpen}
-        placement="right"
         onClose={closeCart}
+        placement="right"
         size="sm"
       >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Your Shopping Cart</DrawerHeader>
-
-          <DrawerBody>
-            {checkout.lineItems?.length ? (
-              checkout.lineItems.map((item) => (
-                <Grid templateColumns="repeat(4, 1fr)" gap={1} key={item.id}>
-                  <Flex alignItems="center" justifyContent="center">
-                    <CloseIcon
-                      cursor="pointer"
-                      onClick={() => removeLineItem(item.id)}
-                    />
-                  </Flex>
-                  <Flex alignItems="center" justifyContent="center">
-                    <Image src={item.variant.image.src} />
-                  </Flex>
-                  <Flex alignItems="center" justifyContent="center">
-                    <Text> {item.title}</Text>
-                  </Flex>
-                  <Flex alignItems="center" justifyContent="center">
-                    <Text> {item.variant.price}</Text>
-                  </Flex>
-                </Grid>
-              ))
-            ) : (
-              <Box h="100%" w="100%">
-                <Text
-                  h="100%"
-                  display="flex"
-                  flexDir="column"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  Your Cart is empty!
-                </Text>
-              </Box>
-            )}
-          </DrawerBody>
-
-          {checkout.lineItems?.length ? (
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Your Shopping Cart</DrawerHeader>
+            <DrawerBody>
+              {checkout?.lineItems?.length ? (
+                <SimpleGrid columns={1} spacing={10}>
+                  {checkout.lineItems &&
+                    checkout.lineItems.map((item) => (
+                      <Grid
+                        templateColumns="repeat(4, 1fr)"
+                        gap={1}
+                        key={item.id}
+                      >
+                        <Box
+                          display="flex"
+                          alignItems="center"
+                          justifyContent="center"
+                        >
+                          <CloseIcon
+                            cursor="pointer"
+                            onClick={() => removeLineItem(item.id)}
+                          />
+                        </Box>
+                        <Box
+                          display="flex"
+                          alignItems="center"
+                          justifyContent="center"
+                        >
+                          <Image src={item.variant.image.src} />
+                        </Box>
+                        <Box
+                          display="flex"
+                          flexDir="column"
+                          align="center"
+                          justify="center"
+                        >
+                          <Text fontSize="sm" fontWeight="bold">
+                            {item.title}
+                          </Text>
+                          <Text fontSize="sm">{item.variant.title}</Text>
+                        </Box>
+                        <Box>
+                          <Text
+                            height="100%"
+                            display="flex"
+                            align="center"
+                            justifyContent="center"
+                          >
+                            {item.variant.price}
+                          </Text>
+                        </Box>
+                      </Grid>
+                    ))}
+                </SimpleGrid>
+              ) : (
+                <Box h="100%" w="100%">
+                  <Text
+                    h="100%"
+                    display="flex"
+                    flexDir="column"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    Your Cart is empty!
+                  </Text>
+                </Box>
+              )}
+            </DrawerBody>
             <DrawerFooter>
-              <Button colorScheme="blue" w="100%">
-                <Link w="100%" href={checkout.webUrl}>
+              <Button w="100%">
+                <Link w="100%" href={checkout?.webUrl}>
                   Checkout
                 </Link>
               </Button>
             </DrawerFooter>
-          ) : null}
-        </DrawerContent>
+          </DrawerContent>
+        </DrawerOverlay>
       </Drawer>
     </>
   );
